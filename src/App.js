12663d9da6eccbedb5b464e8fdaac82772c0e42e
David Betancur <davo.beta.fer@gmail.com>
2024-04-08 22:11:42 -0500

    add link to online store and tiktok buttons

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
         <img src={logo} className="logo" alt="logo" />
         <div class="d-grid gap-4 col-8 mx-auto">
           <button class="btn btn-color" type="button">Whatsapp</button>
-          <button class="btn btn-color" type="button">Tienda Online</button>
-          <button class="btn btn-color" type="button">TikTok</button>
+          <a href="https://bitescol.com" class="btn btn-color" type="button">Tienda Online</a>
+          <a href="https://www.tiktok.com/@bitesbymilahs" class="btn btn-color" type="button">TikTok</a>
         </div>
         <footer><img src={footerImg} className='footer-img' alt="footer"/></footer>
     </div>
