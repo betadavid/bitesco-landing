274d36c7f236bff8c516499070ddf702e1feafd3
David Betancur <davo.beta.fer@gmail.com>
2024-04-08 23:54:52 -0500

    working on responsiveness

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import footerImg from "./footer.png";
 
 function App() {
   return (
-    <div className="app">
+    <div className="app d-flex flex-column align-items-start">
         <img src={logo} className="logo" alt="logo" />
         <div class="d-grid gap-4 col-8 mx-auto">
           <button class="btn btn-color" type="button">Whatsapp</button>
           <a href="https://bitescol.com" class="btn btn-color" type="button">Tienda Online</a>
-          <a href="https://www.tiktok.com/@bitesbymilahs" class="btn btn-color" type="button">TikTok</a>
+          <a href="https://www.tiktok.com/@bitesbymilahs" class="btn btn-color mb-10" type="button">TikTok</a>
         </div>
-        <footer><img src={footerImg} className='footer-img' alt="footer"/></footer>
+        <img src={footerImg} className="footer-img" alt="footer"/>
     </div>
   );
 }
