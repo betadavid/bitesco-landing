304f79262e10d6c61d33dfc2b70b8c32a784964a
David Betancur <davo.beta.fer@gmail.com>
2024-04-09 19:03:05 -0500

    add whatsapp link, fix css layout

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="app d-flex flex-column align-items-start">
         <img src={logo} className="logo" alt="logo" />
         <div class="d-grid gap-4 col-8 mx-auto">
-          <button class="btn btn-color" type="button">Whatsapp</button>
+          <a href="https://wa.me/message/SJKUMZR4SBJYJ1" class="btn btn-color" type="button">Whatsapp</a>
           <a href="https://bitescol.com" class="btn btn-color" type="button">Tienda Online</a>
           <a href="https://www.tiktok.com/@bitesbymilahs" class="btn btn-color mb-10" type="button">TikTok</a>
         </div>
