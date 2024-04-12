b2b07b552c7b5eecfb00261df96f7e2afe940182
David Betancur <davo.beta.fer@gmail.com>
2024-04-11 22:51:29 -0500

    update rappi links

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
     <div className="app d-flex flex-column align-items-start">
         <img src={logo} className="logo" alt="logo" />
         <div class="d-grid gap-4 col-8 mx-auto">
-          <a href="https://wa.me/c/573053198052" class="btn btn-color" type="button">Whatsapp</a>
-          <a href="https://bitescol.com" class="btn btn-color" type="button">Tienda Online</a>
-          <a href="https://www.tiktok.com/@bitesbymilahs" class="btn btn-color mb-10" type="button">TikTok</a>
+          <a href="https://wa.me/c/573053198052" class="btn btn-color" type="button"><strong>Whatsapp</strong></a>
+          <a href="https://bitescol.com" class="btn btn-color" type="button"><strong>Tienda Online</strong></a>
+          <a href="https://rappi.app.link/i2E0caXA9Hb" class="btn btn-color" type="button"><strong>Rappi Medellin</strong></a>
+          <a href="https://rappi.app.link/NzGlQbCKlHb" class="btn btn-color" type="button"><strong>Rappi Bogotá</strong></a>
+          <a href="https://www.tiktok.com/@bitesbymilahs" class="btn btn-color mb-10" type="button"><strong>TikTok</strong></a>
         </div>
         <img src={footerImg} className="footer-img" alt="footer"/>
     </div>
